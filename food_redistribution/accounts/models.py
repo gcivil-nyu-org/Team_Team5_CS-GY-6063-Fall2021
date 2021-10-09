014082c067e78bea4bc89c841f10919c0a20ea0f
Satyen Singh <srs833@nyu.edu>
2021-10-09 18:57:51 -0400

    created Custom User Model for different types of users(Restaurant, Food Redistributor, Individual) and migrated to database

--- food_redistribution/accounts/models.py
+++ food_redistribution/accounts/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    
+    is_restaurant = models.BooleanField(default=False)
+    is_food_redistributor = models.BooleanField(default=False)
+    is_individual = models.BooleanField(default=False)
+
+class Restaurant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=500)
+    email = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500)
+    address = models.CharField(max_length=500)
+
+class FoodRedistributor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=500)
+    email = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500)
+    address = models.CharField(max_length=500)
+
+class Individual(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=500)
+    email = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500)
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
+  
+    
