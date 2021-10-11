3f985e78fdca2a21defa63a8176d94347472d736
HenryTseTse <henrytse5433@gmail.com>
2021-10-10 21:45:48 -0400

    First Draft Demo
    Registration Page, Login Page, Dashboard Page

--- food_redistribution/accounts/admin.py
+++ food_redistribution/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import FoodRedistributor, Restaurant
+
+admin.site.register(FoodRedistributor)
+admin.site.register(Restaurant)
--- food_redistribution/accounts/models.py
+++ food_redistribution/accounts/models.py
@@ -1,32 +1,21 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-class User(AbstractUser):
-    
-    is_restaurant = models.BooleanField(default=False)
-    is_food_redistributor = models.BooleanField(default=False)
-    is_individual = models.BooleanField(default=False)
-
-class Restaurant(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    name = models.CharField(max_length=500)
-    email = models.CharField(max_length=500)
-    phone = models.CharField(max_length=500)
-    address = models.CharField(max_length=500)
+# Create your models here.
 
 class FoodRedistributor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    name = models.CharField(max_length=500)
-    email = models.CharField(max_length=500)
-    phone = models.CharField(max_length=500)
-    address = models.CharField(max_length=500)
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-class Individual(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    name = models.CharField(max_length=500)
-    email = models.CharField(max_length=500)
-    phone = models.CharField(max_length=500)
-    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
-  
-    
+	def __str__(self):
+		return self.name
+
+class Restaurant(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+	def __str__(self):
+		return self.name
