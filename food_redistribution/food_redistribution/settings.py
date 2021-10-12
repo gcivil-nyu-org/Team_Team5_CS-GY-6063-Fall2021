0f47978129c412986a7dc2bb5caa0999a44b223d
HenryTseTse <henrytse5433@gmail.com>
2021-10-12 17:36:35 -0400

    heroku settings

--- food_redistribution/food_redistribution/settings.py
+++ food_redistribution/food_redistribution/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -129,3 +130,5 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
