e5a0cecfadb7330d5b68de247eef372bd4ba00bf
Satyen Singh <srs833@nyu.edu>
2021-10-14 21:16:49 -0400

    added in password reset templates to replace django's default views and set urls.py to go to those templates

--- food_redistribution/accounts/urls.py
+++ food_redistribution/accounts/urls.py
@@ -10,10 +10,26 @@ urlpatterns = [
     path('foodredislogin/', views.login_foodredistributor, name="login2"),
     path('restuarantlogout/', views.logout_restuarant, name="logout"),
     path('foodredislogout/', views.logout_foodredistributor, name="logout2"),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    
+    path(
+        'reset_password/', 
+        auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), 
+        name="reset_password"),
+
+    path(
+        'reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+        name="password_reset_done"),
+        
+    path(
+        'reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+        name="password_reset_confirm"),
+    
+    path(
+        'reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"), 
+        name="password_reset_complete"),
 
     path('', views.home, name="home"),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,6}-[0-9A-Za-z]{1,32})/$',
