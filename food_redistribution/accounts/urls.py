10f539f0227f47bde89629246c8b2e428dc06de1
Satyen Singh <srs833@nyu.edu>
2021-10-14 20:27:20 -0400

    added in reset password endpoints to urls.py and modified requirements.txt to include dotenv

--- food_redistribution/accounts/urls.py
+++ food_redistribution/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('restaurant/', views.register_restaurant, name='register'),
@@ -9,6 +10,10 @@ urlpatterns = [
     path('foodredislogin/', views.login_foodredistributor, name="login2"),
     path('restuarantlogout/', views.logout_restuarant, name="logout"),
     path('foodredislogout/', views.logout_foodredistributor, name="logout2"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
     path('', views.home, name="home"),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,6}-[0-9A-Za-z]{1,32})/$',
