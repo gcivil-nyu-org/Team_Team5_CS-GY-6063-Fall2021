e76ac6229530b7e78549f73b5ad28a52df1de01d
sri-harsh <tr.sriharsha@gmail.com>
2021-10-25 16:46:02 -0400

    error in delete

--- food_redistribution/cal/urls.py
+++ food_redistribution/cal/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from . import views
-
+from .views import DeleteEventView
 app_name = 'cal'
 urlpatterns = [
     url(r'^index/$', views.index, name='index'),
     url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
     url(r'^event/new/$', views.event, name='event_new'),
-	url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
+    url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
+    url('event/<int:pk>/remove', DeleteEventView.as_view(), name='delete_post'),
 ]
--- food_redistribution/cal/views.py
+++ food_redistribution/cal/views.py
@@ -1,17 +1,20 @@
 from datetime import datetime, timedelta, date
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 import calendar
-
+from django.views.generic import DeleteView
 from .models import *
 from .utils import Calendar
 from .forms import EventForm
+from django.urls import reverse_lazy
+
 
 def index(request):
     return HttpResponse('hello')
+
 
 class CalendarView(generic.ListView):
     model = Event
@@ -27,11 +30,13 @@ class CalendarView(generic.ListView):
         context['next_month'] = next_month(d)
         return context
 
+
 def get_date(req_month):
     if req_month:
         year, month = (int(x) for x in req_month.split('-'))
         return date(year, month, day=1)
     return datetime.today()
+
 
 def prev_month(d):
     first = d.replace(day=1)
@@ -39,12 +44,14 @@ def prev_month(d):
     month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
     return month
 
+
 def next_month(d):
     days_in_month = calendar.monthrange(d.year, d.month)[1]
     last = d.replace(day=days_in_month)
     next_month = last + timedelta(days=1)
     month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
     return month
+
 
 def event(request, event_id=None):
     instance = Event()
@@ -54,7 +61,14 @@ def event(request, event_id=None):
         instance = Event()
 
     form = EventForm(request.POST or None, instance=instance)
+
     if request.POST and form.is_valid():
         form.save()
         return HttpResponseRedirect(reverse('cal:calendar'))
     return render(request, 'cal/event.html', {'form': form})
+
+
+class DeleteEventView(DeleteView):
+    model = Event
+    template_name = 'cal/event.html'
+    success_url = reverse_lazy('calendar')
