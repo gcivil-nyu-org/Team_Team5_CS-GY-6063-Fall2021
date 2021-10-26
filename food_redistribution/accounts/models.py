e7b675c0943fd7161132569fa331771d646c6efd
Amber Barksdale <barksdaleaz@gmail.com>
2021-10-25 21:51:35 -0400

    changed travis and reformatted

--- food_redistribution/accounts/models.py
+++ food_redistribution/accounts/models.py
@@ -33,11 +33,11 @@ class Restaurant(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
     def __str__(self):
-        return self.title+'|'+str(self.author)
+        return self.title + "|" + str(self.author)
 
     def get_absolute_url(self):
-        return reverse('blog-details',args=(str(self.id)))
+        return reverse("blog-details", args=(str(self.id)))
