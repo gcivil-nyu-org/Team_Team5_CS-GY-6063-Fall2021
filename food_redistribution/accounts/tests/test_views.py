1f841b8d001985ab24bd8f7f9a86adb0c83481f2
Amber Barksdale <barksdaleaz@gmail.com>
2021-10-29 16:35:40 -0400

    two more simple tests

--- food_redistribution/accounts/tests/test_views.py
+++ food_redistribution/accounts/tests/test_views.py
@@ -1,4 +1,4 @@
-from django.test import TestCase  # , Client
+from django.test import TestCase, Client
 from django.urls import reverse
 
 # from django.contrib.auth.models import User
@@ -43,44 +43,19 @@ class DetailedblogViewTest(TestCase):
         # self.assertContains(response, "No polls are available.")
 """
 
-"""
-class AuthorListViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # Create 13 authors for pagination tests
-        number_of_authors = 13
-
-        for author_id in range(number_of_authors):
-            Author.objects.create(
-                first_name=f'Christian {author_id}',
-                last_name=f'Surname {author_id}',
-            )
-
-    def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/catalog/authors/')
+class ViewTests(TestCase):
+    def test_restaurant_register(self):
+        """
+        A get request to the restaurant registration page should yield a valid response
+        """
+        c = Client()
+        response = c.get(reverse("register"))
         self.assertEqual(response.status_code, 200)
 
-    def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('authors'))
+    def test_food_red_register(self):
+        """
+        A get request to the food redistributor registration page should yield a valid response
+        """
+        c = Client()
+        response = c.get(reverse("register2"))
         self.assertEqual(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('authors'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'catalog/author_list.html')
-
-    def test_pagination_is_ten(self):
-        response = self.client.get(reverse('authors'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('is_paginated' in response.context)
-        self.assertTrue(response.context['is_paginated'] == True)
-        self.assertEqual(len(response.context['author_list']), 10)
-
-    def test_lists_all_authors(self):
-        # Get second page and confirm it has (exactly) remaining 3 items
-        response = self.client.get(reverse('authors')+'?page=2')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('is_paginated' in response.context)
-        self.assertTrue(response.context['is_paginated'] == True)
-        self.assertEqual(len(response.context['author_list']), 3)
-"""
