759721bc7da0ad1ca383f2c62582ea2ca41b446f
Satyen Singh <srs833@nyu.edu>
2021-10-29 21:37:16 -0400

    added test for cal/views.py

--- food_redistribution/cal/tests/test_forms.py
+++ food_redistribution/cal/tests/test_forms.py
@@ -1,1 +1,16 @@
-# from django.test import TestCase
+from django.test import TestCase
+from cal.forms import EventForm
+
+
+class TestForms(TestCase):
+    def test_EventForm_valid_data(self):
+        form = EventForm(
+            data={
+                "start_time": "2021-10-29T19:30",
+                "end_time": "2021-10-29T21:30",
+                "title": "Testing a new event!",
+                "description": "I wanna check if this thing really works",
+            }
+        )
+
+        self.assertTrue(form.is_valid())
