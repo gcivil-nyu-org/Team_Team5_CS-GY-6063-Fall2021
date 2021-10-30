e457115edcf551c616b1bdd5680c656038cb97e4
Satyen Singh <srs833@nyu.edu>
2021-10-29 22:12:41 -0400

    formatted using black

--- food_redistribution/cal/tests/test_models.py
+++ food_redistribution/cal/tests/test_models.py
@@ -3,12 +3,11 @@ from cal.models import Event
 
 
 class TestEvent(TestCase):
-
     def test_setUp(self):
 
         self.event = Event.objects.create(
-            title="new event!", 
-            description="Hey, I am testing a new event!", 
+            title="new event!",
+            description="Hey, I am testing a new event!",
             start_time="2021-10-29T19:30",
             end_time="2021-10-29T21:30",
         )
