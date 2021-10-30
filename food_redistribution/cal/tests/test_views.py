de3ea2e3f5e718f9001bbed2e0d0fd2dacf38556
sri-harsh <tr.sriharsha@gmail.com>
2021-10-30 13:36:24 -0400

    tests views

--- food_redistribution/cal/tests/test_views.py
+++ food_redistribution/cal/tests/test_views.py
@@ -1,1 +1,32 @@
-# from django.test import TestCase
+from django.http import response
+from django.test import TestCase, Client
+from django.urls import reverse
+from cal.models import Event
+
+
+class TestViews(TestCase):
+
+    def test_index(self):
+        client = Client()
+        response = client.get(reverse("cal:index"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_get_date(self):
+        client = Client()
+        response = client.get(reverse("cal:calendar"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_prev_month(self):
+        client = Client()
+        response = client.get(reverse("cal:calendar"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_next_month(self):
+        client = Client()
+        response = client.get(reverse("cal:calendar"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_event(self):
+        client = Client()
+        response = client.get(reverse("cal:event_new"))
+        self.assertEquals(response.status_code, 200)
