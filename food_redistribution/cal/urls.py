b6b701d1ab1a0111b8b891de9a9480a8661c6e33
Satyen Singh <srs833@nyu.edu>
2021-11-03 17:37:29 -0400

    reformatted with black

--- food_redistribution/cal/urls.py
+++ food_redistribution/cal/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = "cal"
 urlpatterns = [
-    url('index/', views.index, name="index"),
-    url('calendar/', views.CalendarView.as_view(), name="calendar"),
-    url('event/new/', views.event_create, name="event_new"),
+    url("index/", views.index, name="index"),
+    url("calendar/", views.CalendarView.as_view(), name="calendar"),
+    url("event/new/", views.event_create, name="event_new"),
     url(r"^event/edit/(?P<pk>\d+)/$", views.event_update, name="event_edit"),
     url(r"^event/delete/(?P<pk>\d+)/$", views.event_delete, name="event_delete"),
 ]
