d86edabeab83d4532821b98c278ee8480e0c01c0
Henry Tse <ht688@nyu.edu>
2021-11-08 00:39:43 -0500

    reformat

--- food_redistribution/accounts/tests/test_views.py
+++ food_redistribution/accounts/tests/test_views.py
@@ -150,28 +150,22 @@ class ViewTests(BaseTest):
             target_status_code=200,
             fetch_redirect_response=True,
         )
-    
+
     def test_wrong_login_restaurant(self):
         response = self.client.post(
             "/restuarantlogin/",
-            data={
-                "username": "test1",
-                "password": "test1" + "test1",
-            },
+            data={"username": "test1", "password": "test1" + "test1",},
             follow=True,
         )
-        self.assertIn("",str(response.content))
-    
+        self.assertIn("", str(response.content))
+
     def test_wrong_login_foodredis(self):
         response = self.client.post(
             "/foodredislogin/",
-            data={
-                "username": "test1",
-                "password": "test1" + "test1",
-            },
+            data={"username": "test1", "password": "test1" + "test1",},
             follow=True,
         )
-        self.assertIn("",str(response.content))
+        self.assertIn("", str(response.content))
 
     def test_food_red_login(self):
         """
