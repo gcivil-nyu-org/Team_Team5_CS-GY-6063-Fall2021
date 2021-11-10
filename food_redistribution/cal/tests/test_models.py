ff35374b81cb27df8d8258c742b7876ab564b16f
Satyen Singh <srs833@nyu.edu>
2021-11-09 23:55:10 -0500

    fixed the tests in cal to reflect updated model

--- food_redistribution/cal/tests/test_models.py
+++ food_redistribution/cal/tests/test_models.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
 from cal.models import Event
+from accounts.models import User
 
 
 class TestEvent(TestCase):
-    def setUp(self):
-        self.event = Event.objects.create(  # pragma: no cover
+    def test_setUp(self):
+        user = User.objects.create_user(
+            username="john", email="jlennon@beatles.com", password="glass onion"
+        )
+        self.event = Event.objects.create(
             title="new event!",
             description="Hey, I am testing a new event!",
             start_time="2021-10-29T19:30",
             end_time="2021-10-29T21:30",
+            author=user,
         )
--- food_redistribution/cal/tests/test_views.py
+++ food_redistribution/cal/tests/test_views.py
@@ -2,15 +2,21 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from unittest.mock import Mock, patch
 from cal.models import Event
+from accounts.models import User
 
 
 class TestViews(TestCase):
     def setUp(self):
+        self.user = User.objects.create_user(
+            username="john", email="jlennon@beatles.com", password="glass onion"
+        )
+
         self.event = Event.objects.create(
             title="new event!",
             description="Hey, I am testing a new event!",
             start_time="2021-10-29T19:30",
             end_time="2021-10-29T21:30",
+            author=self.user,
         )
 
     def test_index(self):
