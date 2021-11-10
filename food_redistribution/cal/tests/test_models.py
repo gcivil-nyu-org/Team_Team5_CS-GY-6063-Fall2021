62472fc0cc0667ab31fb6e4c08cb9ce67a3d4a85
Satyen Singh <srs833@nyu.edu>
2021-11-09 21:42:26 -0500

    added in additional tests for calendar, bump coverage up to 87

--- food_redistribution/cal/tests/test_models.py
+++ food_redistribution/cal/tests/test_models.py
@@ -3,11 +3,14 @@ from cal.models import Event
 
 
 class TestEvent(TestCase):
-    def test_setUp(self):
-
+    def setUp(self):
         self.event = Event.objects.create(
             title="new event!",
             description="Hey, I am testing a new event!",
             start_time="2021-10-29T19:30",
             end_time="2021-10-29T21:30",
         )
+
+    def check_html_url(self):
+        event = Event.objects.get(title="new event!")
+        self.assertEqual(event.get_html_url())
--- food_redistribution/cal/tests/test_views.py
+++ food_redistribution/cal/tests/test_views.py
@@ -1,9 +1,17 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from unittest.mock import Mock, patch
-
+from cal.models import Event
 
 class TestViews(TestCase):
+
+    def setUp(self):
+        self.event = Event.objects.create(
+            title="new event!",
+            description="Hey, I am testing a new event!",
+            start_time="2021-10-29T19:30",
+            end_time="2021-10-29T21:30",
+        )
     def test_index(self):
         client = Client()
         response = client.get(reverse("cal:index"))
@@ -27,6 +35,26 @@ class TestViews(TestCase):
     def test_event(self):
         client = Client()
         response = client.get(reverse("cal:event_new"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_event_view(self):
+        client = Client()
+        response = client.get(reverse("cal:event_view", args=(self.event.pk,)))
+        self.assertEquals(response.status_code, 200)
+
+    def test_event_create(self):
+        client = Client()
+        response = client.get(reverse("cal:event_new"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_event_update(self):
+        client = Client()
+        response = client.get(reverse("cal:event_edit", args=(self.event.pk,)))
+        self.assertEquals(response.status_code, 200)
+
+    def test_event_delete(self):
+        client = Client()
+        response = client.get(reverse("cal:event_delete", args=(self.event.pk,)))
         self.assertEquals(response.status_code, 200)
 
     @patch("cal.views.get_date")
