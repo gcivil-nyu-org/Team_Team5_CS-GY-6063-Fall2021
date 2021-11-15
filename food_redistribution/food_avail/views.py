4dfa275cb09bd4959317395a1c5ac583513a56eb
Satyen Singh <srs833@nyu.edu>
2021-11-15 12:25:51 -0500

    fixed food posting feature, can now create once and update as many times

--- food_redistribution/food_avail/views.py
+++ food_redistribution/food_avail/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from .forms import FoodAvailForm
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.urls import reverse
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -17,20 +17,18 @@ def check_existing_post(request):
         return True
 
 
+@login_required
 def post_available_food(request):
-    instance = Food_Avail()
-    data = request.POST.copy()
-    data["author"] = request.user
-    form = FoodAvailForm(data or None, instance=instance)
-    if request.POST and form.is_valid() and check_existing_post(request):
-        form.save()
-        return HttpResponseRedirect(reverse("accounts:home"))
+    instance = get_object_or_404(Food_Avail, author=request.user)
+    form = FoodAvailForm(request.POST, request.FILES, instance=instance)
+    if request.method == 'POST':
+        form = FoodAvailForm(request.POST or None, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:home')
     else:
-        messages.info(
-            request, "food availability by restaurant has already been posted!"
-        )
-    return render(request, "food_avail/post_food_avail.html", {"food": form})
-
+        form = FoodAvailForm(instance=instance)
+    return render(request, 'food_avail/post_food_avail.html', {'food': form})
 
 def check_food_availibility(request):
     food = Food_Avail.objects.all()
