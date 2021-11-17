880c1a3a7572d65e5930064a3f007fa9c4fc90da
Satyen Singh <srs833@nyu.edu>
2021-11-17 12:22:40 -0500

    changed functions to class based views

--- food_redistribution/food_avail/urls.py
+++ food_redistribution/food_avail/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from food_avail.views import *
+from food_avail import views
 
 app_name = "food_avail"
 
 urlpatterns = [
-    path("post_food_avail/", post_available_food, name="food_avail"),
-    path("food_avail_all/", check_food_availibility, name="view_food_avail"),
-    path("food_avail_res/", view_available_food, name="view_food_avail_res"),
-    path("timeslots/",  create_timeslots, name="create_timeslots"),
+    # path("post_food_avail/", post_available_food, name="food_avail"),
+    path("post_food_avail/", views.FoodAvailCreateView.as_view(), name="post_food_avail"),
+    path("update_food_avail/", views.food_avail_update_view, name="update_food_avail"),
+    path("food_avail_all/", views.check_food_availibility, name="view_food_avail"),
+    path("food_avail_res/", views.view_available_food, name="view_food_avail_res"),
+    path("timeslots/",  views.create_timeslots, name="create_timeslots"),
 
 ]
--- food_redistribution/food_avail/views.py
+++ food_redistribution/food_avail/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from .forms import FoodAvailForm, TimeSlotForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-
+from django.views.generic import CreateView, UpdateView
+from django.views.generic.base import View
+from django.contrib import messages
 # Create your views here.
 
 
@@ -28,31 +30,84 @@ def create_bookings(request):
     return render(request, "food_avail/bookings.html")
 
 
+
+
+# @login_required
+class FoodAvailCreateView(CreateView):
+    model = FoodAvail
+    template_name = "food_avail/post_food_avail.html"
+    form_class = FoodAvailForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.save()
+        return super(FoodAvailCreateView, self).form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super(FoodAvailCreateView, self).get_form_kwargs()
+        kwargs.update({'request': self.request})
+        return kwargs
+
+    def get_success_url(self):
+        messages.add_message(self.request, messages.INFO, 'Food Availability Posted Successfully.')
+        success_url = '/food_avail_res/'
+        return success_url
+
+    def get(self, *args, **kwargs):
+     if len(self.model.objects.filter(author=self.request.user)) > 0:
+        return redirect("food_avail:update_food_avail")
+        # return HttpResponseRedirect(reverse("food_avail:update_food_avail"))
+     else:
+        return super().get(*args, **kwargs)       
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['food'] = FoodAvail.objects.filter(author=self.request.user)
+        return context
+
+# @login_required
+# def post_available_food(request):
+#     if not check_existing_post(request):
+#         instance = FoodAvail()
+#         data = request.POST.copy()
+#         data["author"] = request.user
+#         form = FoodAvailForm(data or None, instance=instance)
+#         if request.POST and form.is_valid():
+#             form.save()
+#             print("Working")
+#             return HttpResponseRedirect(reverse("accounts:home"))
+#     else:
+#         instance = get_object_or_404(FoodAvail, author=request.user)
+#         form = FoodAvailForm(request.POST, request.FILES, instance=instance)
+#         if request.method == "POST":
+#             form = FoodAvailForm(request.POST or None, instance=instance)
+#             if form.is_valid():
+#                 form.save()
+#                 print("Working")
+#                 return HttpResponseRedirect(reverse("food_avail:view_food_avail_res"))
+#                 # return HttpResponseRedirect(reverse("accounts:home"))
+#         else:
+#             form = FoodAvailForm(instance=instance)
+#             print("not working")
+#     return render(request, "food_avail/post_food_avail.html", {"food": form})
+
 @login_required
-def post_available_food(request):
-    if not check_existing_post(request):
-        instance = FoodAvail()
-        data = request.POST.copy()
-        data["author"] = request.user
-        form = FoodAvailForm(data or None, instance=instance)
-        if request.POST and form.is_valid():
+def food_avail_update_view(request):
+    instance = get_object_or_404(FoodAvail, author=request.user)
+    form = FoodAvailForm(request.POST, request.FILES, instance=instance)
+    if request.method == "POST":
+        form = FoodAvailForm(request.POST or None, instance=instance)
+        if form.is_valid():
             form.save()
-            print("Working")
-            return HttpResponseRedirect(reverse("accounts:home"))
-    else:
-        instance = get_object_or_404(FoodAvail, author=request.user)
-        form = FoodAvailForm(request.POST, request.FILES, instance=instance)
-        if request.method == "POST":
-            form = FoodAvailForm(request.POST or None, instance=instance)
-            if form.is_valid():
-                form.save()
-                print("Working")
-                return HttpResponseRedirect(reverse("food_avail:view_food_avail_res"))
-                # return HttpResponseRedirect(reverse("accounts:home"))
+            messages.add_message(request, messages.INFO, 'Food Availability Posted Successfully.')
+            return HttpResponseRedirect(reverse("food_avail:view_food_avail_res"))
         else:
             form = FoodAvailForm(instance=instance)
-            print("not working")
-    return render(request, "food_avail/post_food_avail.html", {"food": form})
+
+    # print("not working")
+    # food_avail = FoodAvail.objects.get(author=request.user)
+    return render(request, "food_avail/update_food_avail.html", {'form': form, 'food': instance})
+    
 
 @login_required
 def view_available_food(request):
@@ -73,7 +128,7 @@ def create_timeslots(request):
         form = TimeSlotForm(data or None, instance=instance)
         if request.POST and form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse("accounts:home"))
+            return HttpResponseRedirect(request.path_info)
     else:
         instance = get_object_or_404(TimeSlot, time_slot_owner=request.user)
         form = TimeSlotForm(request.POST, request.FILES, instance=instance)
@@ -81,8 +136,8 @@ def create_timeslots(request):
             form = TimeSlotForm(request.POST or None, instance=instance)
             if form.is_valid():
                 form.save()
-                return HttpResponseRedirect(reverse("accounts:home"))
+                return HttpResponseRedirect(request.path_info)
         else:
             form = TimeSlotForm(instance=instance)
-    return render(request, "food_avail/timeslots.html", {"timeslot": form})
+    return render(request, "food_avail/view_food_avail.html", {"timeslot": form})
 
