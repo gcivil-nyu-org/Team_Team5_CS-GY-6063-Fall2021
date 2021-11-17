3c897c22135ea7bbfbea0d4302ed2a03293f1f58
Satyen Singh <srs833@nyu.edu>
2021-11-16 21:13:59 -0500

    reformatted and tested travis locally

--- food_redistribution/food_avail/views.py
+++ food_redistribution/food_avail/views.py
@@ -16,8 +16,10 @@ def check_existing_post(request):
     else:
         return False
 
+
 def create_bookings(request):
     return render(request, "food_avail/bookings.html")
+
 
 @login_required
 def post_available_food(request):
